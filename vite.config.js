fa0edf42412ae5622ae248472ffa8c41279e1b24
Cyril John De Leon <deleo.cyriljohn21@gmail.com>
2023-08-09 09:40:00 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,10 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
-});
-// // // 
+  build: {
+    outDir: 'build', // Output built files to the "build" directory
+    // rollupOptions: {
+    //   external: ['react-icons/fa'], // Add other external modules if needed
+    // },
+  },
+})
